5c2fdd8517e5d5a6890ea57f0d6d97f462d58547
Prasun Dev Pal <palprasundev@gmail.com>
2026-06-26 19:29:54 +0530

    Add files via upload

---
+++ Basics/Operators/Arithmatic operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a=10, b=5;
+	cout<< "Sum: "<< (a+b)<<endl;
+	cout<< "Difference: "<< (a-b)<<endl;
+	cout<< "Product: "<< (a*b)<<endl;
+	cout<< "Division: "<< (a/b)<<endl;
+	cout<< "modulo: "<< (a%b)<<endl;
+	cout<< "Division: "<< (a/(float)b)<<endl;	
+	
+	return 0;
+}

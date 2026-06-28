3f812e7f03c67be19b8ee354f2ad02fcbdc6d849
Prasun Dev Pal <palprasundev@gmail.com>
2026-06-28 22:18:34 +0530

    File added

---
+++ Basics/Conditional st. loops and nested loops/check +ve and -ve int.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout << "Enter a number: ";
+	cin>>n;
+	if (n>=0){
+		cout << "n is a Positive number" << endl;
+	} else{
+		cout << "n is a Negetive number" << endl;
+	}
+
+return 0;
+}
+

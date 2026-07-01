1b26a37a820983b9283ad8e73f1caec1b3e6bb68
Prasun Dev Pal <palprasundev@gmail.com>
2026-07-01 18:24:08 +0530

    Add files via upload

---
+++ Basics/Conditional st. loops and nested loops/Loops/PrintNumber1ton.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter the Number: ";
+	cin>> n;
+	
+	int count=1;
+	while( count<=n ){
+		cout<<count<<endl;
+		count++;
+	}
+	return 0;
+}
